22922b3ebc739b873ff6d6d282d45320d0b837a0
Hendrikto <hendrik.to@gmail.com>
2019-11-13 20:34:42 +0100

    Implement function to selectively extract images

--- imgattr/prepare.py
+++ imgattr/prepare.py
@@ -1,3 +1,6 @@
+import os.path
+from zipfile import ZipFile
+
 import pandas as pd
 
 max_image_bytes = 1024 ** 2
@@ -23,3 +26,21 @@ def filter_info(csv_path):
 
     # reset the index
     return info.reset_index(drop=True)
+
+
+def extract_images(input_path, output_path, names):
+    """Selectively extract relevant images from a zip archive."""
+    names = set(names)
+    rejected = set()
+    with ZipFile(input_path) as input_zip, \
+            ZipFile(output_path, 'a') as output_zip:
+        for image_info in input_zip.infolist():
+            image_name = os.path.basename(image_info.filename)
+            if image_name not in names:
+                continue
+
+            if image_info.file_size > max_image_bytes:
+                rejected.add(image_name)
+            else:
+                output_zip.writestr(image_name, input_zip.read(image_info.filename))
+    return rejected
