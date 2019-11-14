5d308ee8b862424f3a97ab7cc9881b6344699ec2
Hendrikto <hendrik.to@gmail.com>
2019-11-15 00:04:53 +0100

    Implement command line interface for image extraction

--- imgattr/cli.py
+++ imgattr/cli.py
@@ -1,6 +1,38 @@
 from argparse import ArgumentParser
+from pathlib import Path
 
-from .prepare import filter_info
+import pandas as pd
+
+from .prepare import (
+    extract_images,
+    filter_info,
+)
+
+extract_images_parser = ArgumentParser(
+    prog='extract_images',
+    description='Extract relevant files from a zip archive.',
+)
+extract_images_parser.add_argument(
+    '-i', '--input',
+    type=Path,
+    required=True,
+    help='path to the input zip file',
+    dest='input_path',
+)
+extract_images_parser.add_argument(
+    '-o', '--output',
+    type=Path,
+    default='data/images.zip',
+    help='path to the output zip file',
+    dest='output_path',
+)
+extract_images_parser.add_argument(
+    '--info',
+    type=Path,
+    default='data/info.csv',
+    help='path to the CSV file with the relevant images',
+    dest='info_path',
+)
 
 filter_info_parser = ArgumentParser(
     prog='filter_info',
@@ -24,6 +56,16 @@ filter_info_parser.add_argument(
 
 class CLICommand:
     @staticmethod
+    def extract_images(info_path, input_path, output_path):
+        print(f"# Reading relevant images from '{info_path}'…")
+        info = pd.read_csv(info_path)
+        print(f"# Extracting relevant images from '{input_path}' into '{output_path}'…")
+        rejected = extract_images(input_path, output_path, info.new_filename)
+        print(f"# Removing rejected images from '{info_path}'…")
+        info = info[~info.new_filename.isin(rejected)]
+        info.to_csv(info_path, index=False)
+
+    @staticmethod
     def filter_info(input_path, output_path):
         print(f'# Filtering info {input_path!r}…')
         info = filter_info(input_path)
@@ -32,6 +74,7 @@ class CLICommand:
 
 
 commands = {
+    'extract_images': (CLICommand.extract_images, extract_images_parser),
     'filter_info': (CLICommand.filter_info, filter_info_parser),
 }
 
