6d4610c0ee3005be49ca5fe6a226116a823693c3
Hendrikto <hendrik.to@gmail.com>
2019-11-17 00:35:22 +0100

    Extract 3D color histogram for all bands at once

--- imgattr/extract_features.py
+++ imgattr/extract_features.py
@@ -10,3 +10,10 @@ def extract_band_histograms(image, bins=32):
         band_hist = np.histogram(band_data, bins=bins, range=(0, 255), density=True)[0]
         histograms[band * bins:(band + 1) * bins] = band_hist
     return histograms
+
+
+def extract_color_histogram(image, bins=10):
+    """Extract flattened 3D color histogram."""
+    data = np.array(image.getdata())
+    histogram = np.histogramdd(data, bins=bins, range=((0, 255),) * 3, density=True)[0]
+    return histogram.ravel()
