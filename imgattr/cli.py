d05758e9b2fd259af98c04b496da166b8cebfb8d
Hendrikto <hendrik.to@gmail.com>
2019-11-17 20:45:03 +0100

    Implement command line interface for resizing images

--- imgattr/cli.py
+++ imgattr/cli.py
@@ -6,6 +6,7 @@ import pandas as pd
 from .prepare import (
     extract_images,
     filter_info,
+    resize_images,
 )
 
 extract_images_parser = ArgumentParser(
@@ -53,6 +54,25 @@ filter_info_parser.add_argument(
     dest='output_path',
 )
 
+resize_images_parser = ArgumentParser(
+    prog='resize_images',
+    description='Resize images to fit into a maximum size.',
+)
+resize_images_parser.add_argument(
+    '-i', '--input',
+    type=Path,
+    default='data/images.zip',
+    help='path to the input zip file',
+    dest='input_path',
+)
+resize_images_parser.add_argument(
+    '-o', '--output',
+    type=Path,
+    default='data/images-resized.zip',
+    help='path to the output zip file',
+    dest='output_path',
+)
+
 
 class CLICommand:
     @staticmethod
@@ -72,10 +92,16 @@ class CLICommand:
         print(f'# Saving filtered info as {output_path!r}…')
         info.to_csv(output_path, index=False)
 
+    @staticmethod
+    def resize_images(input_path, output_path):
+        print(f"# Resizing images from '{input_path}' into '{output_path}'…")
+        resize_images(input_path, output_path)
+
 
 commands = {
     'extract_images': (CLICommand.extract_images, extract_images_parser),
     'filter_info': (CLICommand.filter_info, filter_info_parser),
+    'resize_images': (CLICommand.resize_images, resize_images_parser),
 }
 
 command_parser = ArgumentParser(
