8a389bb98b2c9c84292de8358472c4161b48d941
Hendrikto <hendrik.to@gmail.com>
2020-01-28 02:20:45 +0100

    Extract palette information

--- imgattr/extract_features.py
+++ imgattr/extract_features.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 import numpy as np
 
 
@@ -24,10 +26,30 @@ def extract_extrema(image):
     return np.array(image.getextrema()).ravel()
 
 
+def extract_palette_info(image):
+    """Extract information about the color palette."""
+    palette = Counter(image.getdata())
+    n_colors = len(palette)
+    most_frequent_colors = palette.most_common(3)
+    return np.array((
+        # palette size
+        n_colors,
+        # fractions of most frequent colors
+        most_frequent_colors[0][1] / n_colors,
+        most_frequent_colors[1][1] / n_colors,
+        most_frequent_colors[2][1] / n_colors,
+        # fraction of unique colors
+        sum(1 for value in palette.values() if value == 1) / n_colors,
+        # mean color frequency
+        sum(palette.values()) / n_colors,
+    ))
+
+
 def extract_features(image):
     """Extract features from an image."""
     return np.concatenate((
         extract_band_histograms(image),
         extract_color_histogram(image),
         extract_extrema(image),
+        extract_palette_info(image),
     ))
